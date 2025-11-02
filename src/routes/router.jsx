9b694ea2f3c686c49674b5bed028b07e10a675fe
MH-liton-360 <liton72524nk@gmail.com>
2025-11-02 21:35:35 +0600

    page setup done

--- src/routes/router.jsx
+++ src/routes/router.jsx
@@ -1,7 +1,22 @@
 import { createBrowserRouter } from "react-router-dom";
 
 const router = createBrowserRouter([
-
+    {
+        path: "/",
+        element: <h1>Landing page</h1>
+    },
+    {
+        path: "/details",
+        element: <h1>Details page</h1>
+    },
+    {
+        path: "auth",
+        element: <h1>Login page</h1>
+    },
+    {
+        path: "*",
+        element: <h1>Error page</h1>
+    }
 ])
 
 export default router;
